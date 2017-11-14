85ce04c523d5be5cfc51710f2ed8397a388a96f4
Min Nam <nsm12889@gmail.com>
2017-11-14 15:00:01 -0800

    use this as a template

--- src/legacy/Peril.Noise.js
+++ src/legacy/Peril.Noise.js
@@ -1,77 +1,91 @@
-// Peril.js 
-// Library for Web Audio API
-// Developed by Min Nam
-// jabuem.co
+/**
+ * To Play create a html file and write following
+ * 
+ * <html>
+ * <body>
+ * <script>
+ * ######## CODE BELOW ############
+ * </script>
+ * </body>
+ * </html>
+ * 
+ */
 
-var Noise = function(type, level){
-	this.type      = type;
-	this.level     = level;
-	this.buffer    = null;
-	this.callback  = null;
-	this.processor = null;
-	this.output    = null;
-	this.b         = [];
+/** Followings are constant */
+var AC = new (window.AudioContext || window.webkitAudioContext)()
+var PI = Math.PI
+var TWOPI = 2.0 * PI
+var SAMPLERATE = AC.sampleRate
+
+/** This is class */
+var Noise = function (type, level) {
+  this.type = type
+  this.level = level
+  this.buffer = null
+  this.callback = null
+  this.processor = null
+  this.output = null
+  this.b = []
+
+  this.init()
 }
 
-Noise.prototype.init = function(){
-	this.setType(this.type);
-	console.log("Noise Created");
-	this.b[0] = this.b[1] = this.b[2] = this.b[3] = this.b[4] = this.b[5] = this.b[6] = 0.0;
-	
-	this.processor = AC.createScriptProcessor(0, 0, 1);
-    this.processor.onaudioprocess = this.process.bind(this);
+Noise.prototype.init = function () {
+  this.setType(this.type)
 
-    this.output = AC.createGain();    
+  this.b[0] = this.b[1] = this.b[2] = this.b[3] = this.b[4] = this.b[5] = this.b[6] = 0.0
 
-	this.output.gain.value = this.level;
+  this.processor = AC.createScriptProcessor(0, 0, 1)
+  this.processor.onaudioprocess = this.process.bind(this)
 
-    this.processor.connect(this.output);
+  this.output = AC.createGain()
+
+  this.output.gain.value = this.level
+
+  this.processor.connect(this.output)
 }
 
-Noise.prototype.setType = function(type){
-	this.type = type;
-	switch(this.type){
-		case 0:
-			this.callback = this.getWhiteTick;
-			break;
-		case 1: 
-			this.callback = this.getPinkTick;
-
-	}
+Noise.prototype.setType = function (type) {
+  this.type = type
+  switch (this.type) {
+    case 0:
+      this.callback = this.getWhiteTick
+      break
+    case 1:
+      this.callback = this.getPinkTick
+  }
 }
 
-Noise.prototype.process = function(event){
+Noise.prototype.process = function (event) {
+  var outputArray = event.outputBuffer.getChannelData(0)
+  var bufferSize = outputArray.length
 
-    var outputArray = event.outputBuffer.getChannelData(0);
-    var bufferSize = outputArray.length;
-        
-    for (i = 0; i < bufferSize; i++)
-    {                
-        outputArray[i] = this.callback();
-    }
+  for (var i = 0; i < bufferSize; i++) {
+    outputArray[i] = this.callback()
+  }
 }
 
-Noise.prototype.getWhiteTick = function(){
-	var val = Math.random() * 2 - 1;
-    return val;
+Noise.prototype.getWhiteTick = function () {
+  var val = Math.random() * 2 - 1
+  return val
 }
 
-Noise.prototype.getPinkTick = function(){
-	var val;
-	var white = this.getWhiteTick();
-	this.b[0] = 0.99886 * this.b[0] + white * 0.0555179;
-	this.b[1] = 0.99332 * this.b[1] + white * 0.0750759;
-	this.b[2] = 0.96900 * this.b[2] + white * 0.1538520;
-	this.b[3] = 0.86650 * this.b[3] + white * 0.3104856;
-	this.b[4] = 0.55000 * this.b[4] + white * 0.5329522;
-	this.b[5] = -0.7616 * this.b[5] - white * 0.0168980;
-		val = (this.b[0] + this.b[1] + this.b[2] + this.b[3] + this.b[4] + this.b[5] + this.b[6] + white * 0.5362) * 0.1;
-		
-	this.b[6] = white;
-    return val;
+Noise.prototype.getPinkTick = function () {
+  var val
+  var white = this.getWhiteTick()
+  this.b[0] = 0.99886 * this.b[0] + white * 0.0555179
+  this.b[1] = 0.99332 * this.b[1] + white * 0.0750759
+  this.b[2] = 0.96900 * this.b[2] + white * 0.1538520
+  this.b[3] = 0.86650 * this.b[3] + white * 0.3104856
+  this.b[4] = 0.55000 * this.b[4] + white * 0.5329522
+  this.b[5] = -0.7616 * this.b[5] - white * 0.0168980
+
+  val = (this.b[0] + this.b[1] + this.b[2] + this.b[3] + this.b[4] + this.b[5] + this.b[6] + white * 0.5362) * 0.1
+
+  this.b[6] = white
+  return val
 }
 
+var noise = new Noise(0, 0.1)
 
-
-
-		
+noise.output.connect(AC.destination)
